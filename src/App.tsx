f7eac15c6b851cd75db2de2408413d105d7d320b
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-20 12:40:54 +0530

    1st Section Hard Coded and Color Button Hard Coded

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ColorButton from "./components/ColorButton";
 import NameAndDetails from "./components/NameAndDetails";
 
 function App() {
@@ -6,6 +7,11 @@ function App() {
       <h1 className="mt-8 text-3xl text-center font-bold">Resume Builder</h1>
       <div className="p-8 mx-10 my-14 xl:mx-56 border-2 border-gray-300">
         <NameAndDetails />
+      </div>
+      <div className="flex align-middle justify-evenly w-1/4 m-auto">
+        <ColorButton color="orange" />
+        <ColorButton color="blue" />
+        <ColorButton color="green" />
       </div>
     </>
   );
---
+++ src/components/ColorButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ColorButton = (props: any) => {
+  return (
+    <div
+      className={`w-8 h-8 bg-${props.color}-600 rounded-full cursor-pointer`}
+    ></div>
+  );
+};
+
+export default ColorButton;
