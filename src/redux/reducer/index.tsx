463179beb60a2c940a654d0aed0f035d5ff67456
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-25 15:35:04 +0530

    1st Section edit functionality and adding redux

--- src/redux/reducer/index.tsx
+++ src/redux/reducer/index.tsx
@@ -1,5 +1,26 @@
 import { combineReducers } from "redux";
+// import { useState } from "react";
 
-const rootReducer = combineReducers({});
+// const [edit, setEdit] = useState<any>({
+//   name: "Your Name",
+//   position: "Your Position",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit.Architecto quaerat cupiditate sapiente iste odio veniam dolorum blanditiis incidunt?Praesentium laborum nobis ipsa modi cumque voluptates, iure aliquid asperiores sit. Ad.",
+//   email: "example@domain.com",
+//   phone: 1234567890,
+//   city: "City, Country",
+//   linkedin: "linkedin.com/in/username",
+// });
+
+const postsReducer = (state = [], action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  posts: postsReducer,
+});
 
 export default rootReducer;
