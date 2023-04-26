4b8fe61741e5625ad9afbea359716e7da1323a25
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-26 17:43:49 +0530

    1st Section edit functionality and adding redux

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ function App() {
       <h1 className="mt-8 text-3xl text-center font-bold">Resume Builder</h1>
       <div className="p-8 mx-10 my-14 xl:mx-56 border-2 border-gray-300">
         <NameAndDetails />
-        <div className="flex justify-between flex-wrap w-full mt-10">
+        {/* <div className="flex justify-between flex-wrap w-full mt-10">
           <div className="w-full md:w-6/12 font-Poppins">
             <WorkExperinece />
           </div>
-        </div>
+        </div> */}
       </div>
 
       {/* <div className="flex align-middle justify-evenly w-1/4 m-auto">
